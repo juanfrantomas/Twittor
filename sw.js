8b608b35905dce26c7bbb198bdc2c70f379e2eeb
Juanfran Tomas <juanfrandeveloper@gmail.com>
2019-09-24 16:01:45 +0200

    fix two ios mobile

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE      = 'static-v1';
-const DYNAMIC_CACHE     = 'dynamic-v1';
+const STATIC_CACHE      = 'static-v2';
+const DYNAMIC_CACHE     = 'dynamic-v2';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
 
@@ -47,6 +47,9 @@ self.addEventListener('activate', e => {
         keys.forEach(key => {
 
             if (key !== STATIC_CACHE && STATIC_CACHE.split("-")[0] === key.split("-")[0]) {
+                return caches.delete(key);
+            }
+            if (key !== DYNAMIC_CACHE && DYNAMIC_CACHE.split("-")[0] === key.split("-")[0]) {
                 return caches.delete(key);
             }
 
